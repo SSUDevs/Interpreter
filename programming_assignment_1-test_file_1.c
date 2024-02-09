23795bb5443a66a621620e60652706b9da14d737
Luis <luiscarmon123@gmail.com>
2024-02-08 20:24:58 -0800

    Added a way to convert file to vector

---
+++ programming_assignment_1-test_file_1.c
@@ -0,0 +1,13 @@
+// ***************************************************
+// * CS460: Programming Assignment 1: Test Program 1 *
+// ***************************************************
+procedure main (void)
+{
+  int counter;
+
+  counter = 2;
+/* 
+  counter = 100;
+*/
+  printf ("counter = %d\n", counter);
+}
