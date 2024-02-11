79793985411fb9d86197359370a3a5b4a6d4d508
Ben Harris <harrisbe@sonoma.edu>
2024-02-11 14:48:04 -0800

    Corrected programming_assignment_1-test_file_1.c

--- programming_assignment_1-test_file_1.c
+++ programming_assignment_1-test_file_1.c
@@ -1,9 +1,13 @@
-/****************************************************
- *  CS460: Programming Assignment 1: Test Program 5 *
- ****************************************************/
+// ***************************************************
+// * CS460: Programming Assignment 1: Test Program 1 *
+// ***************************************************
 procedure main (void)
 {
-    int counter;
-    */
-    counter = 100 / 2;
+  int counter;
+
+  counter = 2;
+/* 
+  counter = 100;
+*/
+  printf ("counter = %d\n", counter);
 }
