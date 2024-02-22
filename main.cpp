30e78327f67302e746bc7c2b94a60e90e8ad1bd7
harrisb002 <harrisbe@sonoma.edu>
2024-02-22 13:44:39 -0800

    Update makefile and main

--- main.cpp
+++ main.cpp
@@ -16,25 +16,6 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-	// open file
-	ifstream codeFile;
-	//string fileName = "programming_assignment_1-test_file_1.c";
-	codeFile.open(argv[1]);
-
-	// check if file opened
-	if (!codeFile.is_open()) {
-		cerr << "Couldn't open file: " << argv[1] << endl;
-		exit(0);
-	}
-
-	// get char count in file
-	codeFile.seekg(0, codeFile.end);
-	int charCount = codeFile.tellg();
-	codeFile.seekg(0, codeFile.beg);	// go back to beginning of file
-
-	// close file
-	codeFile.close();
-
     fileAsArray fileArray(argv[1]);
     fileArray.readFile();
 
@@ -47,7 +28,11 @@ int main(int argc, char *argv[]) {
     }
 
     // Print the modified content without comments
-    fileArray.printVector();
+    // fileArray.printVector();
+
+
+	// 
 
     return 0;
 }
+ 
