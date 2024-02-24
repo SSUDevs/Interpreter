90f35a3959f04911cb47637c60984845d4359219
harrisb002 <harrisbe@sonoma.edu>
2024-02-23 18:29:15 -0800

    Added formatting config

--- Token.h
+++ Token.h
@@ -5,28 +5,54 @@
 #include <string>
 
 class Token {
-public:
+  public:
     // Define an enum class for token types
     enum class Type {
-        LParen, RParen, LBracket, RBracket, LBrace, RBrace,
-        DoubleQuote, SingleQuote, Comma, Colon, Semicolon,
-        HexDigit, Digit, AssignmentOperator, Plus, Minus,
-        Slash, Asterisk, Modulo, Caret,
-        Lt, Gt, LtEqual, GtEqual,
-        BooleanAnd, BooleanOr, BooleanNot,
-        BooleanEqual, BooleanNotEqual,
-        Escape, Identifier, String,
-        SingleQuotedString, DoubleQuotedString,
-        WholeNumber, Integer,
+        LParen,
+        RParen,
+        LBracket,
+        RBracket,
+        LBrace,
+        RBrace,
+        DoubleQuote,
+        SingleQuote,
+        Comma,
+        Colon,
+        Semicolon,
+        HexDigit,
+        Digit,
+        AssignmentOperator,
+        Plus,
+        Minus,
+        Slash,
+        Asterisk,
+        Modulo,
+        Caret,
+        Lt,
+        Gt,
+        LtEqual,
+        GtEqual,
+        BooleanAnd,
+        BooleanOr,
+        BooleanNot,
+        BooleanEqual,
+        BooleanNotEqual,
+        Escape,
+        Identifier,
+        String,
+        SingleQuotedString,
+        DoubleQuotedString,
+        WholeNumber,
+        Integer,
         Unknown // For unrecognized tokens
     };
 
-private:
+  private:
     Type _type;
     std::string _value;
 
-public:
-    Token(Type type, const std::string& value) : _type(type), _value(value) {}
+  public:
+    Token(Type type, const std::string &value) : _type(type), _value(value) {}
 
     // Accessors functions
     Type type() const { return _type; }
--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <sstream>
 
-Tokenizer::Tokenizer(const std::vector<char>& file) : _file(file), _size(file.size()) 
-{
-}
-
+Tokenizer::Tokenizer(const std::vector<char> &file)
+    : _file(file), _size(file.size()) {}
 
 void Tokenizer::tokenizeVector() {
     _currentPos = 0;
-    _currentState = START; 
+    _currentState = START;
     while (_currentPos < _size) {
         Token token = getToken(); // Gonna get the next token
-        if (token.type() != Token::Type::Unknown) { // Skiping unknown tokens for now
+        if (token.type() !=
+            Token::Type::Unknown) { // Skiping unknown tokens for now
             _tokens.push_back(token);
         }
     }
@@ -26,57 +25,62 @@ Token Tokenizer::getToken() {
     while (_currentPos < _size && !tokenFound) {
         char currentChar = _file[_currentPos];
         switch (_currentState) {
-            case START:
-                if (std::isspace(currentChar)) {
-                    // Ignore whitespace
-                } else if (std::isalpha(currentChar)) {
-                    tokenType = Token::Type::Identifier;
-                    _currentState = IDENTIFIER;
-                    tokenValue += currentChar;
-                } else if (std::isdigit(currentChar)) {
-                    tokenType = Token::Type::WholeNumber; // I think needs to be updated cause a sign could come before (so for ints)
-                    _currentState = NUMBER;
-                    tokenValue += currentChar;
-                } else {
-                    // Handling for single char tokens (ops, punctuation, ect...)
-                    switch (currentChar) {
-                        case '(':
-                            tokenType = Token::Type::LParen;
-                            tokenValue = currentChar;
-                            tokenFound = true;
-                            break;
-                        case ')':
-                            tokenType = Token::Type::RParen;
-                            tokenValue = currentChar;
-                            tokenFound = true;
-                            break;
-                        // Add cases for other single-character tokens here
-                        default:
-                            tokenType = Token::Type::Unknown;
-                            tokenValue = currentChar;
-                            tokenFound = true;
-                            break;
-                    }
-                }
-                break;
-            case IDENTIFIER:
-                if (std::isalnum(currentChar) || currentChar == '_') {
-                    tokenValue += currentChar;
-                } else {
-                    _currentState = START; // End of identifier (Which is just a string realle)
+        case START:
+            if (std::isspace(currentChar)) {
+                // Ignore whitespace
+            } else if (std::isalpha(currentChar)) {
+                tokenType = Token::Type::Identifier;
+                _currentState = IDENTIFIER;
+                tokenValue += currentChar;
+            } else if (std::isdigit(currentChar)) {
+                tokenType =
+                    Token::Type::WholeNumber; // I think needs to be updated
+                                              // cause a sign could come before
+                                              // (so for ints)
+                _currentState = NUMBER;
+                tokenValue += currentChar;
+            } else {
+                // Handling for single char tokens (ops, punctuation, ect...)
+                switch (currentChar) {
+                case '(':
+                    tokenType = Token::Type::LParen;
+                    tokenValue = currentChar;
                     tokenFound = true;
-                    --_currentPos; // Re-evaluate the character in the next state
-                }
-                break;
-            case NUMBER:
-                if (std::isdigit(currentChar)) { // Keep appending as long as its a number
-                    tokenValue += currentChar;
-                } else {
-                    _currentState = START; // Ending number
+                    break;
+                case ')':
+                    tokenType = Token::Type::RParen;
+                    tokenValue = currentChar;
                     tokenFound = true;
-                    --_currentPos; // Re-evaluate this character in the next state
+                    break;
+                // Add cases for other single-character tokens here
+                default:
+                    tokenType = Token::Type::Unknown;
+                    tokenValue = currentChar;
+                    tokenFound = true;
+                    break;
                 }
-                break;
+            }
+            break;
+        case IDENTIFIER:
+            if (std::isalnum(currentChar) || currentChar == '_') {
+                tokenValue += currentChar;
+            } else {
+                _currentState =
+                    START; // End of identifier (Which is just a string realle)
+                tokenFound = true;
+                --_currentPos; // Re-evaluate the character in the next state
+            }
+            break;
+        case NUMBER:
+            if (std::isdigit(
+                    currentChar)) { // Keep appending as long as its a number
+                tokenValue += currentChar;
+            } else {
+                _currentState = START; // Ending number
+                tokenFound = true;
+                --_currentPos; // Re-evaluate this character in the next state
+            }
+            break;
 
             // Handle STRING state as well as others
         }
@@ -91,6 +95,4 @@ Token Tokenizer::getToken() {
 }
 
 // Method to return the tokens vector
-std::vector<Token> Tokenizer::getTokens() const {
-    return _tokens;
-}
+std::vector<Token> Tokenizer::getTokens() const { return _tokens; }
