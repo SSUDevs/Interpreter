56e14ff115e7e6eb28d9c6840d261cab482a7de2
Zeroxa <2991971hz@gmail.com>
2024-02-24 16:38:16 -0800

    Update Tokenizer.cpp
    following tokens added:
    ::= <
    ::= >
    <LT_EQUAL> ::= <=
    <GT_EQUAL> ::= >=
    <BOOLEAN_AND> ::= &&
    <BOOLEAN_OR> ::= ||
    <BOOLEAN_NOT> ::= !
    <BOOLEAN_EQUAL> ::= ==
    <BOOLEAN_NOT_EQUAL> ::= !=

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -105,11 +105,6 @@ Token Tokenizer::getToken() {
                     tokenValue = currentChar;
                     tokenFound = true;
                     break;
-                case '=':
-                    tokenType = Token::Type::AssignmentOperator;
-                    tokenValue = currentChar;
-                    tokenFound = true;
-                    break;
                 case '*':
                     tokenType = Token::Type::Asterisk;
                     tokenValue = currentChar;
@@ -129,6 +124,59 @@ Token Tokenizer::getToken() {
                     tokenType = Token::Type::Caret;
                     tokenValue = currentChar;
                     tokenFound = true;
+                    break;
+                case '<':
+                    if (_currentPos + 1 < _size && _file[_currentPos + 1] == '=') {
+                    tokenType = Token::Type::LtEqual; 
+                    tokenValue = "<=";
+                    tokenFound = true;
+                    ++_currentPos; 
+                    } else {
+                        tokenType = Token::Type::Lt;
+                        tokenValue = "<";
+                        tokenFound = true;
+                    }
+                    break;
+                 case '>':
+                    if (_currentPos + 1 < _size && _file[_currentPos + 1] == '=') {
+                        tokenType = Token::Type::GtEqual; 
+                        tokenValue = ">=";
+                        tokenFound = true;
+                        ++_currentPos; 
+                    } else {
+                        tokenType = Token::Type::Gt;
+                        tokenValue = ">";
+                        tokenFound = true;
+                    }
+                    break;
+                case '&':
+                    if (_currentPos + 1 < _size && _file[_currentPos + 1] == '&') {
+                        tokenType = Token::Type::BooleanAnd; 
+                        tokenValue = "&&";
+                        tokenFound = true;
+                        ++_currentPos; 
+                    }
+                    break;
+
+                case '|':
+                    if (_currentPos + 1 < _size && _file[_currentPos + 1] == '|') {
+                        tokenType = Token::Type::BooleanOr; 
+                        tokenValue = "||";
+                        tokenFound = true;
+                        ++_currentPos; 
+                    }
+                    break;
+                 case '!':
+                    if (_currentPos + 1 < _size && _file[_currentPos + 1] == '=') {
+                        tokenType = Token::Type::BooleanNotEqual; 
+                        tokenValue = "!=";
+                        tokenFound = true;
+                        ++_currentPos; 
+                    } else {
+                        tokenType = Token::Type::BooleanNot;
+                        tokenValue = "!";
+                        tokenFound = true;
+                    }
                     break;
                 case '+':
                 case '-':
