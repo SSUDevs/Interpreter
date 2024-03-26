ae19254fd087da7fb654b166d2502e7acc93a5d5
harrisb002 <harrisbe@sonoma.edu>
2024-03-25 18:38:01 -0700

    Refactoring Parameter parsing

--- src/CST/Parser.h
+++ src/CST/Parser.h
@@ -33,7 +33,7 @@ private:
 
 
     void parseProcedure();
-    NodePtr parseParameterList();
+    void parseParameterList();
 
     void parseFunction();
 
@@ -49,42 +49,15 @@ private:
     void parseIterationStatement();     // not done
     void parsePrintfStatement();        // not done
     void parseReturnStatement();        // not done
+    void parseSelectionStatement();     // For IF statements
 
-
-    // Starting with this piece of the BNF
-    // <EXPRESSION> ::= <BOOLEAN_EXPRESSION> | <NUMERICAL_EXPRESSION> 
+ 
     void parseExpression();
 
-    void parseSelectionStatement(); // For IF statements
-
-    // NOTES:
-    // <NUMERICAL_EXPRESSION> ::= <NUMERICAL_OPERAND> | <L_PAREN> 
-    // <NUMERICAL_OPERAND> <R_PAREN> | <NUMERICAL_OPERAND> 
-    // <NUMERICAL_OPERATOR> <NUMERICAL_EXPRESSION> | <L_PAREN> 
-    // <NUMERICAL_OPERAND> <NUMERICAL_OPERATOR> <NUMERICAL_EXPRESSION> 
-    // <R_PAREN> | <NUMERICAL_OPERAND> <NUMERICAL_OPERATOR> <L_PAREN> 
-    // <NUMERICAL_EXPRESSION> <R_PAREN> <NUMERICAL_OPERAND> 
-    // <NUMERICAL_OPERATOR> <NUMERICAL_EXPRESSION> | <L_PAREN> 
-    // <NUMERICAL_OPERAND> <NUMERICAL_OPERATOR> <NUMERICAL_EXPRESSION> 
-    // <R_PAREN> | <NUMERICAL_OPERAND> <NUMERICAL_OPERATOR> <L_PAREN> 
-    // <NUMERICAL_EXPRESSION> <R_PAREN>
     void parseNumericalExpression();
-
-    // NOTES:
-    // <BOOLEAN_EXPRESSION> ::= 
-    // <BOOLEAN_TRUE> | <BOOLEAN_FALSE> | <IDENTIFIER> | 
-    // <IDENTIFIER> <BOOLEAN_OPERATOR> <BOOLEAN_EXPRESSION> |  
-    // <L_PAREN> <IDENTIFIER> <BOOLEAN_OPERATOR> <BOOLEAN_EXPRESSION> <R_PAREN> | 
-    // <NUMERICAL_EXPRESSION> <BOOLEAN_EQUAL> <NUMERICAL_EXPRESSION> | 
-    // <NUMERICAL_EXPRESSION> <BOOLEAN_NOT_EQUAL> <NUMERICAL_EXPRESSION> | 
-    // <NUMERICAL_EXPRESSION> <LT_EQUAL> <NUMERICAL_EXPRESSION> | 
-    // <NUMERICAL_EXPRESSION> <GT_EQUAL> <NUMERICAL_EXPRESSION> | 
-    // <NUMERICAL_EXPRESSION> <LT> <NUMERICAL_EXPRESSION> | 
-    // <NUMERICAL_EXPRESSION> <GT> <NUMERICAL_EXPRESSION>
-    void parseBooleanExpression();
-
     void parseNumericalOperand();
 
+    void parseBooleanExpression();
     bool isBooleanOperator(Token::Type type);
     bool isBooleanValue(const std::string& value);
     bool isNumericalOperator(Token::Type type);
